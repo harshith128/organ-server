25a0cdf4006cff99ba846de9157773233e8c4311
HARSHITH C R <88575764+harshith128@users.noreply.github.com>
2021-11-14 16:46:33 +0530

    Update server.js
    testing

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const app = require("./index")
 
 const connect = require("./configs/db")
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, async function(){
+  await connect();
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
